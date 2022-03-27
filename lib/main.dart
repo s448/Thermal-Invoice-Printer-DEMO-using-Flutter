092926b65f6961a425117cb2e565fe9f55522d0b
s448 <saeed01125940652@gmail.com>
2022-03-27 15:03:47 +0200

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -16,16 +20,37 @@ class _MyAppState extends State<MyApp> {
   List<BluetoothDevice> devices = [];
   BluetoothDevice? selectedDevice;
   BlueThermalPrinter printer = BlueThermalPrinter.instance;
-
+  Uint8List? bytes;
+  bool loaded = false;
   @override
   void initState() {
     super.initState();
+    getImage();
     getDevices();
+  }
+
+  /*
+  arabic Invoice require printer that support arabic language , otherwise you can return the invoice as an image 
+  them convert it to unit8List them print it
+  */
+  String url =
+      "http://mohamedmostafa88-001-site4.etempurl.com/test/Print?TransId=0201000001";
+
+  //Convert the Network image to unit8List Image
+  Future<void> getImage() async {
+    try {
+      bytes = (await NetworkAssetBundle(Uri.parse(url)).load(url))
+          .buffer
+          .asUint8List();
+    } catch (e) {
+      // print(e.toString());
+      Get.snackbar(e.toString(), "");
+    }
   }
 
   void getDevices() async {
     devices = await printer.getBondedDevices();
-    setState(() {});
+    // setState(() {});
   }
 
   @override
@@ -37,23 +62,26 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Center(
-              child: DropdownButton<BluetoothDevice>(
-                  value: selectedDevice,
-                  hint: const Text("Select Device"),
-                  items: devices
-                      .map(
-                        (e) => DropdownMenuItem(
-                          child: Text(e.name!),
-                          value: e,
-                        ),
-                      )
-                      .toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      selectedDevice = val;
-                    });
-                  }),
+            SizedBox(
+              width: Get.width,
+              child: Center(
+                child: DropdownButton<BluetoothDevice>(
+                    value: selectedDevice,
+                    hint: const Text("Select Device"),
+                    items: devices
+                        .map(
+                          (e) => DropdownMenuItem(
+                            child: Text(e.name!),
+                            value: e,
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        selectedDevice = val;
+                      });
+                    }),
+              ),
             ),
             const SizedBox(
               height: 20,
@@ -76,21 +104,60 @@ class _MyAppState extends State<MyApp> {
               child: const Text("Disconnect"),
             ),
             ElevatedButton(
+                onPressed: () async {
+                  try {
+                    getImage().then((value) {
+                      setState(() {
+                        loaded = true;
+                      });
+                    });
+                  } catch (e) {
+                    Get.snackbar("ERROR", e.toString(),
+                        duration: const Duration(minutes: 1));
+                  }
+                },
+                child: const Text("Get Image")),
+            ElevatedButton(
               onPressed: () async {
                 if ((await printer.isConnected)!) {
                   try {
-                    printer.printNewLine();
-                    printer.printCustom("Test Message in English", 1, 1);
-                    printer.printCustom("اختبار اللغة العربية", 1, 1);
-                    printer.printQRcode("textToQR", 20, 20, 1);
+                    //Image Byte Print
+                    printer.printImageBytes(
+                      bytes!,
+                    );
+                    //QR Code
+                    printer.printQRcode("Test QR", 60, 60, 1);
+                    //English
+                    printer.printCustom("Test Engilsh Message", 50, 1,
+                        charset: 'UTF-8');
+                    /*
+                    Arabic Text Require Charset to be UTF-16 for the text 
+                    and the thermal printer
+                    */
+                    printer.printCustom("اختبار النص العربي", 50, 1,
+                        charset: 'UTF-16');
                   } catch (e) {
+                    Get.snackbar("ERROR", e.toString(),
+                        duration: const Duration(minutes: 1));
                     print("Connected but not printing " + e.toString());
                   }
                 } else {
+                  Get.snackbar("Printer is not connected", "",
+                      duration: const Duration(minutes: 1));
                   print("Not Connected");
                 }
               },
               child: const Text("Print"),
+            ),
+            SizedBox(
+              width: Get.width,
+              height: Get.height / 3,
+              child: loaded == true
+                  ? Image.memory(
+                      bytes!,
+                      fit: BoxFit.fitHeight,
+                    )
+                  : const Text("Please Click on Get Image Button"),
             )
           ],
         ),
